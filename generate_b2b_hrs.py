1eede5392707fc32db66ac943e9fb59c6e0b8a2d
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-06-20 18:24:00 -0400

    Update generate_b2b_hrs.py

--- generate_b2b_hrs.py
+++ generate_b2b_hrs.py
@@ -8,7 +8,7 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 event_files = []
 
-for year in range(1910, 2025):
+for year in range(1910, 2026):
 
     zip_file = os.path.join(DATA_DIR, f"{year}eve.zip")
     year_dir = os.path.join(DATA_DIR, str(year))
