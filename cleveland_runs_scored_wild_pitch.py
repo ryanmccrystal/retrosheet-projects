7d61cb2d7c36906efc2a294490dea7ce71d7bc56
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-01 17:37:20 -0400

    Update cleveland_runs_scored_wild_pitch.py

--- cleveland_runs_scored_wild_pitch.py
+++ cleveland_runs_scored_wild_pitch.py
@@ -8,8 +8,8 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 event_files = []
 
-# Download 2016-2025 Retrosheet event files
-for year in range(2017, 2026):
+# Download 2000-2025 Retrosheet event files
+for year in range(2000, 2026):
 
     zip_file = os.path.join(DATA_DIR, f"{year}eve.zip")
     year_dir = os.path.join(DATA_DIR, str(year))
