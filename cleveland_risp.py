58f77f79f03afa381213838a76c3272826c02070
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-23 17:43:40 -0400

    Update cleveland_risp.py

--- cleveland_risp.py
+++ cleveland_risp.py
@@ -23,91 +23,91 @@ for year in range(
 
     for game in schedule:
 
-    gamePk = game["game_id"]
-
-    boxscore = statsapi.get(
-        "game",
-        {
-            "gamePk": gamePk
-        }
-    )
-
-    home_id = boxscore["gameData"]["teams"]["home"]["id"]
-
-    if home_id == 114:
+        gamePk = game["game_id"]
     
-        info = (
-            boxscore["liveData"]
-            ["boxscore"]
-            ["teams"]
-            ["home"]
-            ["info"]
+        boxscore = statsapi.get(
+            "game",
+            {
+                "gamePk": gamePk
+            }
         )
     
-        opponent = (
-            boxscore["gameData"]
-            ["teams"]
-            ["away"]
-            ["abbreviation"]
-        )
+        home_id = boxscore["gameData"]["teams"]["home"]["id"]
     
-        home_away = "Home"
-    
-    else:
-    
-        info = (
-            boxscore["liveData"]
-            ["boxscore"]
-            ["teams"]
-            ["away"]
-            ["info"]
-        )
-    
-        opponent = (
-            boxscore["gameData"]
-            ["teams"]
-            ["home"]
-            ["abbreviation"]
-        )
-    
-        home_away = "Away"
-
-        risp = None
-        hits = None
-        at_bats = None
-    
-        for section in info:
-    
-            for field in section.get("fieldList", []):
-    
-                if field["label"] == "Team RISP":
-    
-                    risp = field["value"]
-    
-                    if risp:
-    
-                        match = re.search(
-                            r"(\d+)-for-(\d+)",
-                            risp
-                        )
-    
-                        if match:
-    
-                            hits = int(
-                                match.group(1)
-                            )
-    
-                            at_bats = int(
-                                match.group(2)
-                            )
-    
-        if (
-            hits == 0
-            and at_bats >= 7
-        ):
-    
-            print(
-                f"{game['game_date']}  "
-                f"{home_away} vs {opponent}  "
-                f"{hits}-for-{at_bats}"
+        if home_id == 114:
+        
+            info = (
+                boxscore["liveData"]
+                ["boxscore"]
+                ["teams"]
+                ["home"]
+                ["info"]
             )
+        
+            opponent = (
+                boxscore["gameData"]
+                ["teams"]
+                ["away"]
+                ["abbreviation"]
+            )
+        
+            home_away = "Home"
+        
+        else:
+        
+            info = (
+                boxscore["liveData"]
+                ["boxscore"]
+                ["teams"]
+                ["away"]
+                ["info"]
+            )
+        
+            opponent = (
+                boxscore["gameData"]
+                ["teams"]
+                ["home"]
+                ["abbreviation"]
+            )
+        
+            home_away = "Away"
+    
+            risp = None
+            hits = None
+            at_bats = None
+        
+            for section in info:
+        
+                for field in section.get("fieldList", []):
+        
+                    if field["label"] == "Team RISP":
+        
+                        risp = field["value"]
+        
+                        if risp:
+        
+                            match = re.search(
+                                r"(\d+)-for-(\d+)",
+                                risp
+                            )
+        
+                            if match:
+        
+                                hits = int(
+                                    match.group(1)
+                                )
+        
+                                at_bats = int(
+                                    match.group(2)
+                                )
+        
+            if (
+                hits == 0
+                and at_bats >= 7
+            ):
+        
+                print(
+                    f"{game['game_date']}  "
+                    f"{home_away} vs {opponent}  "
+                    f"{hits}-for-{at_bats}"
+                )
