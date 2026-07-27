1efb86000c2abd76cb7dc364da9d658087c04766
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-27 13:56:56 -0400

    Update cleveland_risp_spans.py

--- cleveland_risp_spans.py
+++ cleveland_risp_spans.py
@@ -125,89 +125,89 @@ for sample_file in event_files:
 
     for row in reader:
 
-    cleveland_batting = (
-
-        (
-            row["AWAY_TEAM_ID"] == TEAM
-            and
-            row["BAT_HOME_ID"] == "0"
+        cleveland_batting = (
+    
+            (
+                row["AWAY_TEAM_ID"] == TEAM
+                and
+                row["BAT_HOME_ID"] == "0"
+            )
+    
+            or
+    
+            (
+                row["GAME_ID"][:3] == TEAM
+                and
+                row["BAT_HOME_ID"] == "1"
+            )
+    
         )
-
-        or
-
-        (
-            row["GAME_ID"][:3] == TEAM
-            and
-            row["BAT_HOME_ID"] == "1"
-        )
-
-    )
-
-    if not cleveland_batting:
-        continue
-
-    if current_game is None:
-
-        current_game = row["GAME_ID"]
-
-        current_home_team = current_game[:3]
-
-        current_away_team = row["AWAY_TEAM_ID"]
-
-    if row["GAME_ID"] != current_game:
-
-        game_date = (
-            f"{current_game[3:7]}-"
-            f"{current_game[7:9]}-"
-            f"{current_game[9:11]}"
-        )
-
-        home_away = (
-            "Home"
-            if current_home_team == TEAM
-            else "Away"
-        )
-
-        opponent = (
-            current_away_team
-            if home_away == "Home"
-            else current_home_team
-        )
-
-        games.append(
-            {
-                "date": game_date,
-                "game_id": current_game,
-                "opponent": opponent,
-                "home_away": home_away,
-                "hits": risp_hits,
-                "ab": risp_ab
-            }
-        )
-
-        current_game = row["GAME_ID"]
-
-        current_home_team = current_game[:3]
-
-        current_away_team = row["AWAY_TEAM_ID"]
-
-        risp_hits = 0
-
-        risp_ab = 0
-
-    if (
-        row["BASE2_RUN_ID"]
-        or
-        row["BASE3_RUN_ID"]
-    ):
-
-        if row["AB_FL"] == "T":
-
-            risp_ab += 1
-
-            if int(row["H_CD"]) > 0:
-
-                risp_hits += 1
+    
+        if not cleveland_batting:
+            continue
+    
+        if current_game is None:
+    
+            current_game = row["GAME_ID"]
+    
+            current_home_team = current_game[:3]
+    
+            current_away_team = row["AWAY_TEAM_ID"]
+    
+        if row["GAME_ID"] != current_game:
+    
+            game_date = (
+                f"{current_game[3:7]}-"
+                f"{current_game[7:9]}-"
+                f"{current_game[9:11]}"
+            )
+    
+            home_away = (
+                "Home"
+                if current_home_team == TEAM
+                else "Away"
+            )
+    
+            opponent = (
+                current_away_team
+                if home_away == "Home"
+                else current_home_team
+            )
+    
+            games.append(
+                {
+                    "date": game_date,
+                    "game_id": current_game,
+                    "opponent": opponent,
+                    "home_away": home_away,
+                    "hits": risp_hits,
+                    "ab": risp_ab
+                }
+            )
+    
+            current_game = row["GAME_ID"]
+    
+            current_home_team = current_game[:3]
+    
+            current_away_team = row["AWAY_TEAM_ID"]
+    
+            risp_hits = 0
+    
+            risp_ab = 0
+    
+        if (
+            row["BASE2_RUN_ID"]
+            or
+            row["BASE3_RUN_ID"]
+        ):
+    
+            if row["AB_FL"] == "T":
+    
+                risp_ab += 1
+    
+                if int(row["H_CD"]) > 0:
+    
+                    risp_hits += 1
 
     if current_game is not None:
     
